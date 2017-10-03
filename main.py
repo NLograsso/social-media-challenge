e39c7c5e281de0bbd7e9c7ca93ca2c80921d0196
Logan Lasiter <lasiterlogan@gmail.com>
2017-10-02 22:03:16 -0700

    removing key

--- main.py
+++ main.py
@@ -2,32 +2,21 @@ import tweepy
 
 auth = tweepy.OAuthHandler('5EEbrv2FAqd0J6t92RTTfbAdc', 'QDxf4Z2FVlVxaO1lsi3uX0e69LbOoNjcjLgZ1pBBmZBYMhULfC')
 
-try:
-    redirect_url = auth.get_authorization_url()
-except tweepy.TweepError:
-    print ('Error! Failed to get request token.')
-
-# Example w/o callback (desktop)
-# verifier = raw_input('Verifier:')
-
-# print(auth.access_token)
-# print(auth.access_token_secret)
-
-auth.set_access_token('24435877-x2wDohtnf7LsQjZ0ZsU18roCDNgivE6rzr9GeRn8G', 'cpzzjk73RkfThQXeHHgAyvmg4MHqRb1nSuEJzNSRPeaWe')
+# try:
+#     redirect_url = auth.get_authorization_url()
+# except tweepy.TweepError:
+#     print ('Error! Failed to get request token.')
 
 api = tweepy.API(auth)
 
-public_tweets = api.home_timeline()
-for tweet in public_tweets:
-    print tweet.text
+# public_tweets = api.home_timeline()
+# for tweet in public_tweets:
+#     print tweet.text
 
+user = api.get_user('realDonaldTrump')
 
-print "THIS IS A TEST THAT NICK WANTS TO TRY"
+print(user.screen_name)
+print(user.followers_count)
 
-user = api.get_user('twitter')
-
-print user.screen_name
-print user.followers_count
-
-for friend in user.friends():
-	print friend.screen_name
+# for tweet in user.tweets():
+# 	print(tweet.text)
